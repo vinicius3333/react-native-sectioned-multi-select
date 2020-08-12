ce45b408771e2adb23861b2598ecd2f4669cae24
Ren Rizzolo <ren@jewishnews.net.au>
2020-08-12 11:54:20 +1000

    simplify provider usage with SMSProvider export

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import SectionedMultiSelect from './lib/sectioned-multi-select'
 
 export { useSectionedMultiSelect } from './lib/useSectionedMultiSelect'
-export { default as SMSContext } from './lib/context/SMSContext'
+export { default as SMSContext, SMSProvider } from './lib/context/SMSContext'
 export { default as useSMSContext } from './lib/context/useSMSContext'
 
 export * from './lib/components'
